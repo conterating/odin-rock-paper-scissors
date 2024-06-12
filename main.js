9da785359766897ff0682e6f6af8603a8a3180f8
Aashay Gupte <151937841+conterating@users.noreply.github.com>
2024-06-12 17:16:14 -0400

    Implented computer choice function
    Randomly selects the choice of the computer

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+function getComputerChoice() {
+  let computer_choice = "";
+  let random_number = Math.random();
+  if (random_number < 1 / 3) {
+    computer_choice = "rock";
+  } else if (random_number >= 1 / 3 && random_number <= 2 / 3) {
+    computer_choice = "paper";
+  } else {
+    computer_choice = "scissors";
+  }
+  return computer_choice;
+}
+
+console.log(getComputerChoice());
