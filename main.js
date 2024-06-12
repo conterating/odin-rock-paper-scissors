70a68ef5326b58d9857ec50597bf716aa71c1f95
Aashay Gupte <151937841+conterating@users.noreply.github.com>
2024-06-12 18:05:28 -0400

    Added the decidegameWinner function
    The winner of the game can be decided based on
    several different outcomes

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-var humanScore = 0;
-var computerScore = 0;
-
 function getComputerChoice() {
   let computer_choice = "";
-  let random_number = Math.random();
+  const random_number = Math.random();
   if (random_number < 1 / 3) {
     computer_choice = "rock";
   } else if (random_number >= 1 / 3 && random_number <= 2 / 3) {
@@ -16,7 +13,22 @@ function getComputerChoice() {
 
 function getHumanChoice() {
   let human_choice = prompt("Choose: Rock, Paper, or Scissors");
-  return human_choice.toLowerCase();
 }
 
-console.log(getHumanChoice());
+function decideGameWinner(humanChoice, computerChoice) {
+  if (humanChoice == "paper" && computerChoice == "rock") {
+    return `You won! ${humanChoice} beats ${computerChoice}.`;
+  } else if (humanChoice == "rock" && computerChoice == "scissors") {
+    return `You won! ${humanChoice} beats ${computerChoice}.`;
+  } else if (humanChoice == "scissors" && computerChoice == "paper") {
+    return `You won! ${humanChoice} beats ${computerChoice}.`;
+  } else if (humanChoice == "rock" && computerChoice == "paper") {
+    return `You lost! ${computerChoice} beats ${humanChoice}.`;
+  } else if (humanChoice == "scissors" && computerChoice == "rock") {
+    return `You lost! ${computerChoice} beats ${humanChoice}`;
+  } else if (humanChoice == "paper" && computerChoice == "rock") {
+    return `You lost ${computerChoice} beats ${humanChoice}`;
+  }
+}
+
+function playGame(humanChoice, computerChoice) {}
